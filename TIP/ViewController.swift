e6d98d3b71beafae19c1993d7681ce23d4323a2e
Lydia-Lu-code <guipopo5@gmail.com>
2020-05-21 22:36:14 +0800

    Initial Commit

---
+++ TIP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TIP
+//
+//  Created by 維衣 on 2020/5/21.
+//  Copyright © 2020 Lydia-L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
